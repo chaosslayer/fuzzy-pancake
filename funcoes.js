ffeae5c0d053bcdf301462cd0baf1569a9a34725
chaosslayer <110621668+chaosslayer@users.noreply.github.com>
2022-09-19 22:37:30 -0300

    versao incompleta

--- funcoes.js
+++ funcoes.js
@@ -2,15 +2,21 @@ var txttoarray = (text) => {
     var toarray = text.split('');
     return toarray;
 }
-var arraytotxt = (text) => {
-    var toarray = text.split('');
-    return totxt;
+var arraytotxt = (ar) => {
+    var toarray = ar.join('');
+    return toarray;
 }
 var verificarinclusao = (eincluso, comparacao) => {
     var ar = txttoarray(comparacao)
     var bol = ar.includes(eincluso)
     return bol;
 }
-var iserir = (text, posicao) => {
-    
+var iserir = (text, indice, array) => {
+    //posicao comeca no 0
+    var ar = []
+    for (var i = 0; i < array.length + 1; i++){
+        if (i == indice){
+            ar.push()
+        }
+    }
 }
